49469be6c885f1b97ccaa5d434c350a046ef576e
Akil Mullassery Prashant <94084424+akilmp@users.noreply.github.com>
2025-08-03 04:34:30 +1000

    Initialize dbt project with core models and tests

---
+++ dbt/models/dim_unit.sql
@@ -0,0 +1,15 @@
+{{ config(
+    materialized='incremental',
+    partition_by={'field': 'trading_date'},
+    unique_key='unit_id'
+) }}
+
+select
+    unit_id,
+    trading_date,
+    unit_name,
+    region_id
+from raw_dim_unit
+{% if is_incremental() %}
+  where trading_date >= (select coalesce(max(trading_date), '1900-01-01') from {{ this }})
+{% endif %}
